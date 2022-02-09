5af36c00f02c882698e85ab2a8a6f8c2fbc27f85
Vannah Pyatt <pya17001@byui.edu>
2022-02-09 14:54:13 -0700

    edits to director

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,7 +1,32 @@
+from game.word import Word
+from game.jumper import Jumper
+from game.guesser import Guesser
+
+
 class Director:
 
     def __init__(self):
-        pass
+        self.word = Word()
+        self.jumper = Jumper()
+        self.num_guesses = 0
+        
+    def check_in_word(self, guess, word):
+        if guess in word:
+            Word.update_guess(guess)
+        else:
+            Jumper.update_drawing()
+
+    def get_guess(self):
+        letter = input("Enter a letter (a-z): ")
+        guess = Guesser(letter)
+        return guess
 
     def start_game(self):
+        word = self.word
+        while self.num_guesses >=5:
+            guess = get_guess()
+            check_in_word(guess, word)
         pass
+
+    
+    

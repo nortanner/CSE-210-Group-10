b607eefc776571f16fc69b842055e42d8db98c62
Vannah Pyatt <pya17001@byui.edu>
2022-02-09 14:04:30 -0700

    word class

---
+++ jumper/game/word.py
@@ -0,0 +1,38 @@
+import random
+
+class Word:
+
+    def __init__(self):
+        words = ["reign", "absorbed", "savory", "mind", "property", "yawn", "yoke", "toe", "letters", "evanescent", "unequaled", "bashful", "truthful", "aftermath", "striped", "care", "cook", "summer", "wakeful", "little", "jail", "defective", "quilt", "jolly", "blushing"]
+        self.word = words.choice()
+        self.guess_word = ""
+        for i in range(len(self.word)):
+            self.guess_word += "_ "
+    
+    def get_word(self):
+        return self.word
+   
+    def update_guess(self, letter_guess):
+        """This function will run if the letter guessed by the user is in the word."""
+        word_editing = list(self.word)
+        for i in range(len(word_editing)):
+            if letter_guess != word_editing[i]:
+                word_editing[i] = "_ "
+        self.guess_word = "".join(word_editing)
+
+
+
+"""This below is what I used to check if the code worked"""
+#def update_guess(word, letter_guess):
+#    guess_word = ""
+#    for i in range(len(word)):
+#        guess_word += "_ "
+#    word_editing = list(word)
+#    for i in range(len(word_editing)):
+#        if letter_guess != word_editing[i]:
+#            word_editing[i] = "_ "
+#    guess_word = "".join(word_editing)
+#    print(guess_word)
+#
+#update_guess("tiller", "l")
+#

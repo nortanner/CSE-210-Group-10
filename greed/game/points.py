aa1451bf46c58947c35d72b75451c8f86340a5a9
Vannah Pyatt <pya17001@byui.edu>
2022-02-23 14:59:53 -0700

    points in director

--- greed/game/points.py
+++ greed/game/points.py
@@ -1,14 +1,14 @@
 class points:
     def __init__(self):
-        self.current_points = 0
+        self.points = 0
 
     def update_points(self, type):
         if type == "gem":
-            self.current_points += 1
+            self.points += 1
         else:
-            self.current_points -= 1
+            self.points -= 1
 
     def get_points(self):
-        return self.current_points
+        return self.points
 
 
